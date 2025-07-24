b591c97fdefa92dbc2ac0b635fba46d855d8de28
MushfiqPlabon <rahman.mushfiq2000@gmail.com>
2025-07-24 12:02:49 +0600

    Iniating mid-term-exam

--- tasks/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from tasks.models import Task, TaskDetail, Project
-
-# Register your models here.
-admin.site.register(Task)
-admin.site.register(TaskDetail)
-admin.site.register(Project)
