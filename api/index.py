4b13cab79cddfa8da83c0b7c26fd0b25dc5ae225
MushfiqPlabon <rahman.mushfiq2000@gmail.com>
2025-10-17 21:16:53 +0600

    forcing vercel deploy

--- api/index.py
+++ api/index.py
@@ -4,4 +4,4 @@ import sys
 # Add the project root to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from eventMan.wsgi import application
+from eventMan.wsgi import application as handler
